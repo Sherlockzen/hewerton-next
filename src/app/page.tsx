77b5414c7941544732dc1107fc28ce75365fad7a
Hewerton <hewertonlourenco@gmail.com>
2023-06-06 23:42:50 -0300

    style: :art: Add some padding to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ProfileInfo } from './components/profileInfo'
 
 export default function Home() {
   return (
-    <main className=' bg-back-dark w-screen h-screen flex flex-col items-center text-white'>
+    <main className=' bg-back-dark w-screen h-screen flex flex-col items-center text-white p-8'>
       <BtnTheme/>
       <ProfileInfo/>
     </main>

848c29ea41ae61a4104d1a41174655ce61900f91
Hewerton <hewertonlourenco@gmail.com>
2023-06-06 22:41:06 -0300

    style: :lipstick: Using daisyUI on the button

--- src/app/components/btnTheme.tsx
+++ src/app/components/btnTheme.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 export default function BtnTheme() {
   return (
-    <div className=" bg-[#171F26] w-20 h-20 flex flex-col justify-center items-center rounded-full">
+    <button className=" btn btn-neutral border-none bg-[#171F26] w-20 h-20 flex flex-col justify-center items-center rounded-full">
       <Image src="/themeImg.svg" alt='icon button' width={40} height={40}/>
-    </div>
+    </button>
   );
 }

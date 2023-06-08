4567f348de9a6b8ad23cbe821c994a07816a01aa
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 13:13:43 -0300

    feat: :lipstick: Add theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import BtnTheme from './components/btnTheme'
 import { ProfileInfo } from './components/profileInfo'
 import ProfileFacts from './components/profileFacts'
 import ProfileBtn from './components/profileBtn'
+import TabsBtn from './components/tabsBtn'
 
 export default function Home() {
   return (
-    <main className=' bg-back-dark w-[100svw] h-[100svh] flex flex-col items-center text-white p-8 gap-8'>
+    <main data-theme='retro' className=' bg-back-dark w-[100svw] h-[100svh] flex flex-col items-center text-white p-8 gap-8'>
       <BtnTheme/>
       <ProfileInfo/>
       <ProfileFacts/>
       <ProfileBtn/>
+      <TabsBtn/>
     </main>
   )
 }

afad54040c2149c0dc32947689d10a693dc40109
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 11:49:26 -0300

    feat: :art: Create profile btn component

---
+++ src/app/components/profileBtn.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ProfileBtn() {
+  return (
+    <div className=' flex gap-3'>
+        <button className=' btn btn-neutral w-40 bg-[#FFE071] normal-case' >
+            <div className=' flex items-baseline gap-2 text-[#3D3D3D]'>
+                Currículo
+                <img className='' src='/downloadImg.svg'/>
+            </div>
+        </button>
+        <button className=' btn btn-neutral w-40 normal-case'>Contate-me</button>
+    </div>
+  )
+}

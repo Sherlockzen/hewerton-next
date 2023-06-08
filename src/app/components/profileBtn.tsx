85fa8946b779c2eb341b4564690031d44096faa0
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 13:14:28 -0300

    feat: :lipstick: Change some styles

--- src/app/components/profileBtn.tsx
+++ src/app/components/profileBtn.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProfileBtn() {
   return (
     <div className=' flex gap-3'>
-        <button className=' btn btn-neutral w-40 bg-[#FFE071] normal-case' >
+        <button className=' btn btn-primary w-40 normal-case' >
             <div className=' flex items-baseline gap-2 text-[#3D3D3D]'>
                 Currículo
                 <img className='' src='/downloadImg.svg'/>

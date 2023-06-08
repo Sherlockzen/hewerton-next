03d109695b3b3ef906213e8ad6eae981fcce1a67
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 11:15:43 -0300

    feat: :lipstick: Implement mobile view first

--- src/app/components/btnTheme.tsx
+++ src/app/components/btnTheme.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 export default function BtnTheme() {
   return (
-    <button className=" btn btn-neutral border-none bg-[#171F26] w-20 h-20 flex flex-col justify-center items-center rounded-full">
-      <Image src="/themeImg.svg" alt='icon button' width={40} height={40}/>
+    <button className=" btn btn-neutral border-none bg-[#171F26] w-12 h-12 md:w-20 md:h-20 rounded-full p-0">
+      <div className=' w-12 h-12 md:w-20 md:h-20 flex flex-col justify-center items-center'>
+        <img src="/themeImg.svg" className=" w-1/2" />
+      </div>
     </button>
   );
 }

3551d8b061ca3a541bf1ab5f1058515da6b95e57
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 11:48:32 -0300

    feat: :art: Add profilefact and profilebtn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import BtnTheme from './components/btnTheme'
 import { ProfileInfo } from './components/profileInfo'
+import ProfileFacts from './components/profileFacts'
+import ProfileBtn from './components/profileBtn'
 
 export default function Home() {
   return (
     <main className=' bg-back-dark w-[100svw] h-[100svh] flex flex-col items-center text-white p-8 gap-8'>
       <BtnTheme/>
       <ProfileInfo/>
+      <ProfileFacts/>
+      <ProfileBtn/>
     </main>
   )
 }

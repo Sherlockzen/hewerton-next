e40d5b0e2bca049b28d3d913e59ba68948a4e656
Hewerton <hewertonlourenco@gmail.com>
2023-06-08 11:49:02 -0300

    feat: :art: Create profile facts component

---
+++ src/app/components/profileFacts.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ProfileFacts() {
+  return (
+    <div className=' flex gap-3 text-center text-sm'>
+        <p>
+            1+ Ano de experiência      
+        </p>
+        <p>
+            40+ projetos no github
+        </p>
+        <p>
+            Participante do Hacktoberfest
+        </p>
+    </div>
+  )
+}
+
+export default ProfileFacts

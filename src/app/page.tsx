030045c4e3aa316de5caa92692a362771b2ee478
Hewerton <hewertonlourenco@gmail.com>
2023-06-09 16:39:36 -0300

    Feat: removing the PortifolioContent component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import { ProfileInfo } from './components/profileInfo'
 import ProfileFacts from './components/profileFacts'
 import ProfileBtn from './components/profileBtn'
 import TabsBtn from './components/tabsBtn'
+import PortifolioContent from "@/app/components/portifolioContent";
 
 export default function Home() {
   return (
-    <main data-theme='retro' className=' bg-back-dark w-[100svw] h-[100svh] flex flex-col items-center text-white p-8 gap-8'>
+    <main data-theme='retro' className=' bg-back-dark w-[100svw] min-h-[100svh] flex flex-col flex items-center text-white p-8 gap-8'>
       <BtnTheme/>
       <ProfileInfo/>
       <ProfileFacts/>

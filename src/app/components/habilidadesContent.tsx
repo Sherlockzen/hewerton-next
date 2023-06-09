8d4db5b988a28474ae7a1eb48108d71c28317471
Hewerton <hewertonlourenco@gmail.com>
2023-06-09 16:38:25 -0300

    Feat: create habilidades content

---
+++ src/app/components/habilidadesContent.tsx
@@ -0,0 +1,14 @@
+const HabilidadesContent = () => {
+    return (
+        <>
+            <div className=' flex flex-col gap-10 w-screen items-center'>
+                <p className=' badge badge-outline badge-lg'>Javascript/Typescript</p>
+                <p className=' badge badge-outline badge-lg'>React</p>
+                <p className=' badge badge-outline badge-lg'>Node.js</p>
+                <p className=' badge badge-outline badge-lg'>SQL</p>
+
+            </div>
+        </>
+    )
+}
+export default HabilidadesContent;

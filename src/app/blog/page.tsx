229f0506fa7b4ab432f51e1a32a793150eb3eb10
Hewerton <hewertonlourenco@gmail.com>
2023-06-10 15:58:53 -0300

    Feat: creating the blog page of the user posts

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { Articles } from '../interfaces/articles';
+
+export default function Page() {
+    
+  return (
+    <div className=" flex flex-col items-center p-10">
+      <h1 className=" text-5xl">Artigos do Blog</h1>
+    </div>
+  );
+}

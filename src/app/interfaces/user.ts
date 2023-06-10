7ef9543ea37ae63942ccbe43a51074a1321d5e66
Hewerton <hewertonlourenco@gmail.com>
2023-06-10 15:58:02 -0300

    Feat: creating the user interface

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    name: string
+    username: string
+    twitter_username: string
+    github_username: string
+    user_id: number
+    website_url: any
+    profile_image: string
+    profile_image_90: string
+}

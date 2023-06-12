d3ae6d0f2c1456266de6d2be5cfc2e1574e9da2b
Hewerton <hewertonlourenco@gmail.com>
2023-06-12 15:27:40 -0300

    Feat: First fetch on dev.to api, its working

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,24 @@
 import { Articles } from '../interfaces/articles';
 
-export default function Page() {
-    
+export default async function Page() {
+  const data = await fetch('https://dev.to/api/articles?username=danielhe4rt')
+  const articles: Articles[] = await data.json()
+
+  console.log(articles);
+  
   return (
     <div className=" flex flex-col items-center p-10">
       <h1 className=" text-5xl">Artigos do Blog</h1>
+      <div>
+        <h2 className=' text-3xl'>Lista de artigos por título: </h2>
+        <div>
+          {
+            articles.map((article) => (
+              <div>{article.title}</div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }

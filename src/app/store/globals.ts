9ee69568fd0effd6b3b790613c9495dca1255ec5
Hewerton <hewertonlourenco@gmail.com>
2023-06-12 22:39:51 -0300

    Feat: create the zustand storage

---
+++ src/app/store/globals.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand'
+import React from 'react'
+import { persist, createJSONStorage, devtools } from 'zustand/middleware'
+
+interface ThemeState {
+    theme: boolean,
+    changeTheme: () => void
+}
+
+export const useDarkTheme = create<ThemeState>()(
+    devtools(
+        persist((set) => ({
+            theme: true,
+            changeTheme: () => set((state) => ({ theme: !state.theme})),
+        }),
+        {
+            name: 'theme-storage'
+        }
+        )
+    )
+)
+
+// export const useDarkTheme = create<ThemeState>((set) => ({
+//     theme: true,
+//     changeTheme: () => set((state) => ({ theme: !state.theme })),
+// }))

1f3dba078705bae7317c0d738612a7fd3b512d39
Hewerton <hewertonlourenco@gmail.com>
2023-06-12 22:37:33 -0300

    Feat: add some configs for Image component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'dev.to',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/practicaldev/image/fetch/**'
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig

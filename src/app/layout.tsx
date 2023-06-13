e2678d2b34105af8c4bf4965087cf60bd6a952c9
Hewerton <hewertonlourenco@gmail.com>
2023-06-13 01:07:19 -0300

    Feat: inserting a provider component to set the theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { useDarkTheme } from './store/globals'
+import { ProviderTheme } from './components/providerTheme'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const theme = useDarkTheme.getState().theme
 
 export const metadata = {
   title: 'Hewerton',
@@ -15,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ProviderTheme>{children}</ProviderTheme>
+      </body>
     </html>
-  )
+  );
 }

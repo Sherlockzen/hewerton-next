bbaf99d4026ac300f94dd77b719269b7f808c6bf
Hewerton <hewertonlourenco@gmail.com>
2023-06-20 22:32:28 -0300

    Feat: removing useless state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import BtnTheme from './components/btnTheme'
 import { ProfileInfo } from './components/profileInfo'
 import ProfileFacts from './components/profileFacts'
 import ProfileBtn from './components/profileBtn'
 import TabsBtn from './components/tabsBtn'
+import { useState } from 'react'
+
 
 
 export default function Home() {
-
+  const [teste, setTeste] = useState(50);
+  
   return (
     <main className='  w-[100svw] min-h-[100svh] flex flex-col items-center p-8 gap-8'>
       <BtnTheme/>

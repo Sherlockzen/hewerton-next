759f7381b05f59e79039a73885b81fbfc1f5d173
Hewerton <hewertonlourenco@gmail.com>
2023-06-28 23:36:42 -0300

    Feat: add download cv function

--- src/app/components/profileBtn.tsx
+++ src/app/components/profileBtn.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function ProfileBtn() {
   return (
-    <div className=' flex gap-3'>
-        <button className=' btn btn-primary w-40 md:w-72 normal-case'>
-            <div className=' flex items-baseline gap-2'>
-                Currículo
-                <img className='' src='/downloadImg.svg'/>
-            </div>
-        </button>
-        <button className=' btn btn-outline w-40 md:w-72 normal-case'>Contate-me</button>
+    <div className=" flex gap-3">
+      <a
+        href="../../../public/HewertonResume.pdf"
+        download={"HewertonResume.pdf"}
+        className=" btn btn-primary w-40 md:w-72 normal-case"
+      >
+        <div className=" flex items-baseline gap-2">
+          Currículo
+          <img className="" src="/downloadImg.svg" />
+        </div>
+      </a>
+      <button className=" btn btn-outline w-40 md:w-72 normal-case">
+        Contate-me
+      </button>
     </div>
-  )
+  );
 }
